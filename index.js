c7693884fe673a04f6e4015bc01bd5dafd6612e1
himanshu devgan <himanshudevgan04@gmail.com>
2018-01-02 14:34:45 +0530

    server

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-/**
- * Packages to be used
- */
+
 let express = require("express");
-let bodyParser = require("body-parser"); //To read the data from GET/POST request
-let cron = require("node-cron"); //For scheduling tasks
-let fs = require("fs"); //File Stream (used to read, write, delete files on server file system)
+let bodyParser = require("body-parser"); 
+let cron = require("node-cron"); 
+let fs = require("fs"); 
 let https = require('https');
 let config = require("./config.json");
 let mongoose = require("mongoose");
 let cors = require("cors");
 
-/**
- * Server Config
- */
+
 let app = express();
 app.use(cors());
 app.use(bodyParser.json());
@@ -20,9 +16,7 @@ app.use(bodyParser.urlencoded(
     { extended: true }
 ));
 
-/**
- * API Config
- */
+
 let apiURL = "newsapi.org";
 let newsSources = ["abc-news", "bbc-news", "bbc-sport", "espn", "business-insider", "buzzfeed", "cnbc", "cnn"];
 
@@ -32,18 +26,13 @@ function getLastDate() {
     return configFile.updatedOn;
 }
 
-/**
- * MongoDB config
- */
 let mongoDB = "mongodb://127.0.0.1:27017/newsfeed";
 mongoose.connect(mongoDB, { usemongoclient: true });
 mongoose.Promise = global.Promise;
 let db = mongoose.connection;
 db.on("error", console.error.bind(console, 'MongoDB Connection Error: '));
 
-/**
- * Schema for News Feed Document
- */
+
 let Schema = mongoose.Schema;
 
 let NewsFeedSchema = new Schema({
@@ -61,11 +50,7 @@ let NewsFeedSchema = new Schema({
 
 let NewsFeedModel = mongoose.model('NewsFeedModel', NewsFeedSchema);
 
-/**
- * Gets the News Feed from URL
- * @param {*} options : API Options
- * @param {*} callback : Returns the news JSON array
- */
+
 function getNewsFeed(options) {
     console.log(options);
     let req = https.get(options, (response) => {
@@ -76,13 +61,13 @@ function getNewsFeed(options) {
             let body = Buffer.concat(bodyChunks);
             let data = JSON.parse(body);
 
-            //Set the date marker
+           
             let date = new Date();
             date.setHours(date.getHours() + 5);
             date.setMinutes(date.getMinutes() + 30);
             config.updatedOn = date.toISOString();
 
-            //Update date marker in config.json
+            
             fs.writeFileSync("config.json", JSON.stringify(config, null, 2), (err) => {
                 if (err) {
                     return console.log(err);
@@ -92,7 +77,7 @@ function getNewsFeed(options) {
             console.log(data.articles);
 
 
-            //Insert data in MongoDB
+            
             data.articles.forEach(newsFeedItem => {
                 let NewsFeedInstance = new NewsFeedModel(newsFeedItem);
                 NewsFeedInstance.save((err) => {
@@ -136,8 +121,8 @@ app.post("/search", (req, res) => {
     let q = req.param("keyword");
     NewsFeedModel.find(
         {
-            'title': new RegExp(q, "i"), //Imitates like Query
-            'source.id': { $in: req.param("sources") } //This imitates IN clause
+            'title': new RegExp(q, "i"), 
+            'source.id': { $in: req.param("sources") } 
         }
         , (err, NewsFeed) => {
             res.json(NewsFeed);
